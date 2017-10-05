787a039987a4fc40ec7f55fabe81f67519990396
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-05 17:45:30 +0000

    automatically backed up by learn

--- db/migrate/20171005174225_create_movies.rb
+++ db/migrate/20171005174225_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |m|
-      m.string :title 
+      m.string :title
       m.datetime :release_date
-      m.string :director 
-      m.string :lead 
+      m.string :director
+      m.string :lead
       m.boolean :in_theaters
     end
   end
